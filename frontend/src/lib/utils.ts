e1e771b272d7e3305fe297e67c72d220a5b023eb
Thomas Cheung <139344260+ThomasC86@users.noreply.github.com>
2024-07-27 23:12:56 +0000

    crypto initial

---
+++ frontend/src/lib/utils.ts
@@ -0,0 +1,7 @@
+//lib/utils.ts
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
