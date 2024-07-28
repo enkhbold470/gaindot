70eca2edae1ada3cef3982bc11fcffd7909c33a1
Enkhbold Ganbold <53986637+enkhbold470@users.noreply.github.com>
2024-07-28 07:23:42 -0400

    sometimes works, need to fine tune

--- backend/flask/ff_mp4.py
+++ backend/flask/ff_mp4.py
@@ -1,9 +1,8 @@
-from moviepy.editor import VideoFileClip
+import subprocess
 
 
-def convert_webm_to_mp4(input_path, output_path):
-    clip = VideoFileClip(input_path)
-    clip.write_videofile(output_path, codec="libx264", audio_codec="aac")
-
-
-convert_webm_to_mp4("blob.webm", "obt.mp4")
+def convert_webm_to_mp4(webm_path, mp4_path):
+    command = f"ffmpeg -y -i {webm_path} {mp4_path}"
+    subprocess.run(command, shell=True)
+    print("Video converted to mp4:")
+    return mp4_path
--- backend/flask/video.py
+++ backend/flask/video.py
@@ -8,90 +8,50 @@ from ff_mp4 import convert_webm_to_mp4
 import subprocess
 
 load_dotenv(".env")
-folder_path = "./uploads/blob.webm"
-
 
 client = OpenAI(
     api_key=os.getenv("OPENAI_API_KEY"),
 )
-command = f"ffmpeg -i ./uploads/blob.webm ./uploads/blob.mp4"
-subprocess.run(command, shell=True)
-print("Video converted to mp4:  ./uploads/blob.mp4")
-video = cv2.VideoCapture("./uploads/blob.mp4")
 
 
-base64Frames = []
-while video.isOpened():
-    success, frame = video.read()
-    if not success:
-        break
-    _, buffer = cv2.imencode(".jpg", frame)
-    base64Frames.append(base64.b64encode(buffer).decode("utf-8"))
+def visualProcessing(file_path, cd_file_path, prompt):
 
-video.release()
-print(len(base64Frames), "frames read.")
+    convert_webm_to_mp4(file_path, cd_file_path)
 
+    # print("Video converted to mp4:")
+    video = cv2.VideoCapture(cd_file_path)
 
-def save_and_delete_frames(video_path, folder_path):
-    # Create the folder to save frames
-    os.makedirs(folder_path, exist_ok=True)
-
-    video = cv2.VideoCapture(video_path)
-    frame_count = 0
-
+    base64Frames = []
     while video.isOpened():
         success, frame = video.read()
         if not success:
             break
-        frame_path = os.path.join(folder_path, f"frame_{frame_count:04d}.jpg")
-        cv2.imwrite(frame_path, frame)
-        frame_count += 1
+        _, buffer = cv2.imencode(".jpg", frame)
+        base64Frames.append(base64.b64encode(buffer).decode("utf-8"))
 
     video.release()
-    print(f"{frame_count} frames read and saved in {folder_path}")
+    print(len(base64Frames), "frames read.")
 
-    # Delete the folder after use
-    # shutil.rmtree(folder_path)
-    # print(f"Folder {folder_path} and all its contents have been deleted.")
+    PROMPT_MESSAGES = [
+        {
+            "role": "user",
+            "content": [
+                f"{prompt}",
+                *map(
+                    lambda x: {"image": x, "resize": 768},
+                    base64Frames[0 : len(base64Frames) // 2],
+                ),
+            ],
+        },
+    ]
+    params = {
+        "model": "gpt-4o-mini",
+        "messages": PROMPT_MESSAGES,
+        "max_tokens": 200,
+    }
 
-
-PROMPT_MESSAGES = [
-    {
-        "role": "user",
-        "content": [
-            "Tell me which exercise it.",
-            *map(lambda x: {"image": x, "resize": 768}, base64Frames[0::50]),
-        ],
-    },
-]
-params = {
-    "model": "gpt-4o-mini",
-    "messages": PROMPT_MESSAGES,
-    "max_tokens": 200,
-}
-
-result = client.chat.completions.create(**params)
-print(result.choices[0].message.content)
-
-
-# response = client.chat.completions.create(
-#     model="gpt-4o-mini",
-#     messages=[
-#         {
-#             "role": "user",
-#             "content": [
-#                 {"type": "text", "text": "What’s in this image?"},
-#                 {
-#                     "type": "image_url",
-#                     "image_url": {
-#                         "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
-#                         "detail": "high",
-#                     },
-#                 },
-#             ],
-#         }
-#     ],
-#     max_tokens=300,
-# )
-
-# print(response.choices[0].message.content)
+    result = client.chat.completions.create(**params)
+    print(result.choices[0].message.content)
+    os.remove(file_path)
+    os.remove(cd_file_path)
+    return result.choices[0].message.content
