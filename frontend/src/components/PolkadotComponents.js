3adb3075e7bb6d396c2f9bb0f0996b8b9ce2a24a
Enkhbold Ganbold <53986637+enkhbold470@users.noreply.github.com>
2024-08-02 05:20:22 -0700

    auth bug fix

--- frontend/src/components/PolkadotComponents.js
+++ frontend/src/components/PolkadotComponents.js
@@ -55,7 +55,16 @@ export const UserButtonWeb3 = () => {
 export const WalletAddressWeb3 = () => {
   const { accounts, extensionReady, walletAddress } = usePolkadot();
 
-  if (!extensionReady || accounts.length === 0) return null;
+  if (!extensionReady || accounts.length === 0)
+    return (
+      <h1 className="font-bold text-base text-red-500 px-2">
+        No wallet connected, Make sure you have Polkadot.js extension installed
+      </h1>
+    );
 
-  return <div className="text-white">{walletAddress}</div>;
+  return (
+    <div className="font-bold text-base text-green-500 px-2 ">
+      {walletAddress}
+    </div>
+  );
 };
